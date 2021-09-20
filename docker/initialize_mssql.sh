6b24ce4ef86b8afe19c55ce766df03b9459be0b5
Mike Wallace <mwmikewallace@gmail.com>
2021-09-19 21:11:53 -0500

    updated the initialize script to hit explicit ports

--- docker/initialize_mssql.sh
+++ docker/initialize_mssql.sh
@@ -3,8 +3,8 @@ password=ITsC0mpl1cat3d
 
 echo Setting up MSSQL Server Databases...
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $password -i ./docker/init_databases.sql
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $password -i ./docker/carrier_code.sql -v db_name=dev
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $password -i ./docker/ontime_data.sql -v db_name=dev
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $password -i ./docker/carrier_code.sql -v db_name=prod
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $password -i ./docker/ontime_data.sql -v db_name=prod
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/init_databases.sql
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/carrier_code.sql -v db_name=dev
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/ontime_data.sql -v db_name=dev
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/carrier_code.sql -v db_name=prod
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/ontime_data.sql -v db_name=prod
