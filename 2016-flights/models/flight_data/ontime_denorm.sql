d89c11e492f5773e443085eb7f4caef23795eade
Donald Sawyer <donald.sawyer@gmail.com>
2021-09-23 00:32:45 -0500

    logic for arrived_flag complete and tests passing.

--- 2016-flights/models/flight_data/ontime_denorm.sql
+++ 2016-flights/models/flight_data/ontime_denorm.sql
@@ -8,7 +8,13 @@ select
   coalesce(cc.description, 'UNKNOWN') as carrier_desc,
   od.fl_num as flight_number,
   od.origin,
-  od.dest as destination
+  od.dest as destination,
+  CASE
+    WHEN od.cancelled != 0 OR od.diverted != 0
+      THEN 'N'
+    WHEN od.arr_delay is not null THEN 'Y'
+    ELSE NULL
+  END as arrived_flag
 from {{ var("db_name") }}.[{{ var("schema_name") }}].ontime_data od
 left join {{ var("db_name") }}.[{{ var("schema_name") }}].carrier_code cc
   on od.carrier = cc.code
