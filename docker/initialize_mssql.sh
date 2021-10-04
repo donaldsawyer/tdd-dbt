1850c48bf422d3e09104922ed6057057c1b44b8c
Mike <mwmikewallace@gmail.com>
2021-10-04 16:09:34 -0500

    corrected sqlcmd location

--- docker/initialize_mssql.sh
+++ docker/initialize_mssql.sh
@@ -3,11 +3,11 @@ password=ITsC0mpl1cat3d
 
 echo Setting up MSSQL Server Databases...
 
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/init_databases.sql
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/carrier_code.sql -v db_name=dev
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/ontime_data.sql -v db_name=dev
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/sp_CreateOntimeCarrierTable.sql -v db_name=dev
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/carrier_code.sql -v db_name=prod
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/ontime_data.sql -v db_name=prod
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ./docker/sp_CreateOntimeCarrierTable.sql -v db_name=prod
+/usr/local/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ../docker/init_databases.sql
+/usr/local/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ../docker/carrier_code.sql -v db_name=dev
+/usr/local/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ../docker/ontime_data.sql -v db_name=dev
+/usr/local/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ../docker/sp_CreateOntimeCarrierTable.sql -v db_name=dev
+/usr/local/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ../docker/carrier_code.sql -v db_name=prod
+/usr/local/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ../docker/ontime_data.sql -v db_name=prod
+/usr/local/bin/sqlcmd -S 0.0.0.0,1433 -U sa -P $password -i ../docker/sp_CreateOntimeCarrierTable.sql -v db_name=prod
 
